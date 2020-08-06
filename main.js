facdde89f42ee3b68cc65ec0e9dbeac166faf5dc
Nicollas Santos <nicollas_comp@hotmail.com>
2020-08-06 20:23:36 -0300

    Tentando subir no servidor2

--- main.js
+++ main.js
@@ -1,41 +1,10 @@
-import 'reflect-metadata';
-
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
-import routes from './routes';
-import AppError from './errors/AppError';
-
-import './database';
-
 var http = require('http');
 
-const cors = require('cors');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node\n');
+}).listen(process.env.PORT_INDEX);
 
-const app = express();
-const porta = 21176;
+console.log('Server running at :'+process.env.PORT_INDEX);
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
 
-app.use((err, request, response, _) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
-
-  // console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
-
-const baseDir = `${__dirname}/build/`
-app.use(express.static(`${baseDir}`))
-app.get('/', (req, res) => res.sendfile('index.html' , { root : baseDir } ))
-
-app.listen(porta);
