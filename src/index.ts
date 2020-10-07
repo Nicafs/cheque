06feca1b957b663efbdd157a4e6756dbc622ba16
Nicollas Santos <nicollas_comp@hotmail.com>
2020-10-07 08:51:56 -0400

    Teste

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import 'reflect-metadata';
+import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -7,28 +7,14 @@ import AppError from './errors/AppError';
 
 import './database';
 
-const path = require("path");
 const cors = require('cors');
 
 const app = express();
-const porta = process.env.PORT_APP || 21130;
-
-var http = require('http');
+const porta = 21176;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", routes);
-
-const baseDir = `${__dirname}/build/`
-
-//Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
-app.use(express.static('build')); 
-app.use(express.static(`${baseDir}`))
-
-// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', "index.html"));
-});
+app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
@@ -46,6 +32,8 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(porta, () => {
-  console.log(`Escutando na porta: ${porta}!`);
-});
+const baseDir = `${__dirname}/build/`;
+app.use(express.static(`${baseDir}`));
+app.get('/', (req, res) => res.sendfile('index.html', { root: baseDir }));
+
+app.listen(porta);
